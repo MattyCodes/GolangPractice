064f3edabe579a3059833fa827a5dbb67d50ded9
MattyCodes <matt.nick.fagioli@gmail.com>
2022-03-03 13:45:59 -0500

    Update the test file with some better comments around installation instructions.

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	google.golang.org/grpc v1.44.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
 )
--- test.go
+++ test.go
@@ -1,8 +1,16 @@
 // This is just a dummy file to test that my Go installation works.
-
+//
+// Installing Go (on Linux Debian/Ubuntu):
+// $ wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+// $ sudo tar -C /usr/local/ -xzf go1.13.5.linux-amd64.tar.gz
+// 
+// Add the following to the `~/.bashrc`:
+// export PATH=$PATH:/usr/local/go/bin
+// export GO111MODULE=on
+//
 // Installing Protobuf and gRPC Go libraries:
-// $ go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
-// $ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+// $ go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
+// $ go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
 
 // Executable file.
 package main
