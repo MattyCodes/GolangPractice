405fac9501946940db59a3504b020138cbd2a1f3
MattyCodes <matt.nick.fagioli@gmail.com>
2022-03-03 09:16:32 -0500

    Initial commit.

---
+++ test.go
@@ -0,0 +1,11 @@
+// Installing Protobuf and gRPC Go libraries:
+// $ go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
+// $ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, World!")
+}
